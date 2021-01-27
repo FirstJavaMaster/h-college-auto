e6cf11e31d4e6e5429edb14d778c1041ac0271e4
matrix <rick2morty@163.com>
2021-01-27 18:45:15 +0800

    实现驻守的额外奖励

--- bin/Defend.py
+++ bin/Defend.py
@@ -32,12 +32,20 @@ class Defend:
                 print('开始驻守 + %d' % started_num)
                 continue
             if swiped_num >= 2:
-                print('应该没有待驻守任务了, 结束')
+                print('没有待驻守任务了')
                 PageHelper().back()
                 break
             self.adb.swipe(400, 1400, 400, 600)
             time.sleep(1)
             swiped_num = swiped_num + 1
+        # 检查额外奖励
+        extra_award_num = 0
+        while not self.adb.check('images/defend/extra-award-none.png'):
+            # todo 点击固定位置 坐标待确定
+            self.adb.click_position(200, 900)
+            self.adb.wait('images/defend/extra-award-take-confirm.png').click(1)
+            extra_award_num = extra_award_num + 1
+            print('额外奖励 + ', extra_award_num)
 
 
 if __name__ == '__main__':
